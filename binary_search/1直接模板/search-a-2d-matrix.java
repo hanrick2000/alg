4d01a24c1a9ddea7cff81f4dc52d3584ed9d4a47
Wang Yang <khil.wang@gmail.com>
2016-10-27 01:12:46 +0800

    update

--- binary_search/1直接模板/search-a-2d-matrix.java
+++ binary_search/1直接模板/search-a-2d-matrix.java
@@ -1,33 +1,33 @@
 public class Solution {
-  public boolean searchMatrix(int[][] matrix, int target) {
-    if (matrix == null || matrix.length == 0){
-      return false;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0){
+            return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int start = 0;
+        int end = row * column - 1; //变成一维的
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            int number = matrix[mid / column][mid % column]; //行列值通过end计算得到
+            if (number == target) {
+                return true;
+            } else if (number < target) {
+                start = mid;
+            } else if (number > target) {
+                end = mid;
+            }
+        }
+        if (matrix[start / column][start % column] == target) {
+            return true;
+        } else if (matrix[end / column][end % column] == target) {
+            return true;
+        }
+        return false;
     }
-    if (matrix[0] == null || matrix[0].length == 0) {
-      return false;
-    }
-    int row = matrix.length;
-    int column = matrix[0].length;
-    int start = 0;
-    int end = row * column - 1; //变成一维的
-    while (start + 1 < end) {
-      int mid = start + (end - start) / 2;
-      int number = matrix[mid / column][mid % column]; //行列值通过end计算得到
-      if (number == target) {
-        return true;
-      } else if (number < target) {
-        start = mid;
-      } else if (number > target) {
-        end = mid;
-      }
-    }
-    if (matrix[start / column][start % column] == target) {
-      return true;
-    } else if (matrix[end / column][end % column] == target) {
-      return true;
-    }
-    return false;
-  }
 }
 
 /*
--- binary_search/1直接模板/search-in-a-big-sorted-array.java
+++ binary_search/1直接模板/search-in-a-big-sorted-array.java
@@ -1,27 +1,27 @@
 public class Solution {
-  public int searchBigSortedArray(ArrayReader reader, int target) {
-    int index = 1;
-    while (reader.get(index - 1) < target){
-      index = index * 2;   //找到一个>=target的index
+    public int searchBigSortedArray(ArrayReader reader, int target) {
+        int index = 1;
+        while (reader.get(index - 1) < target){
+            index = index * 2;     //找到一个>=target的index
+        }
+        int start = 0;
+        int end = index - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (reader.get(mid) < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (reader.get(start) == target) {
+            return start;
+        }
+        if (reader.get(end) == target) {
+            return end;
+        }
+        return -1;
     }
-    int start = 0;
-    int end = index - 1;
-    while (start + 1 < end) {
-      int mid = start + (end - start) / 2;
-      if (reader.get(mid) < target) {
-        start = mid;
-      } else {
-        end = mid;
-      }
-    }
-    if (reader.get(start) == target) {
-      return start;
-    }
-    if (reader.get(end) == target) {
-      return end;
-    }
-    return -1;
-  }
 }
 
 /*
